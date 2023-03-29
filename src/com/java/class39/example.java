ae174f190576a9fd59243aad9c1d436613fc266e
Temurbek <116052673+TEMURBEKTUMANBAEV@users.noreply.github.com>
2023-03-29 08:34:19 +0300

    Create example.java

---
+++ src/com/java/class39/example.java
@@ -0,0 +1,9 @@
+package com.java.class39;
+
+import java.util.Scanner;
+
+public class example {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
+}
