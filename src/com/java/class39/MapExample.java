11847d2fe9b8c52ccd68fffad44461537d18af25
Temurbek <116052673+TEMURBEKTUMANBAEV@users.noreply.github.com>
2023-03-30 09:56:50 +0300

    new file

--- src/com/java/class39/MapExample.java
+++ src/com/java/class39/MapExample.java
@@ -1,7 +1,19 @@
 package com.java.class39;
 
+import java.util.Map;
+import java.util.HashMap;
+
+
 public class MapExample {
     public static void main(String[] args) {
-        
+        Map<String, Integer> map = new HashMap<>();
+        map.put("Temurbek", 46);
+        map.put("Tair", 58);
+        map.put("Azat", 69);
+        map.put("Chirag", 18);
+        map.put("Madina", 230);
+        map.put("Maxim", 24);
+        System.out.println(map);
+        System.out.println(map.get("Temurbek"));
     }
 }
---
+++ src/com/java/class39/example2.java
@@ -0,0 +1,7 @@
+package com.java.class39;
+
+public class example2 {
+    public static void main(String[] args) {
+
+    }
+}
