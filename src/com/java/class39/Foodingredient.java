7e79b551060a21a365b5ab4bfdf6e3c1bcbed2ff
Temurbek <116052673+TEMURBEKTUMANBAEV@users.noreply.github.com>
2023-03-30 10:33:26 +0300

    new file

---
+++ src/com/java/class39/Foodingredient.java
@@ -0,0 +1,16 @@
+package com.java.class39;
+
+import java.util.Map;
+import java.util.HashMap;
+
+public class Foodingredient {
+    public static void main(String[] args) {
+        Map<String, String> map = new HashMap<>();
+        map.put("Boiled rice", "Raw rice,water,air");
+        map.put("Vegetable curry" ,"Vegetables, salt, oil, water, spices");
+        map.put("Dal Pulses,",  "salt, water, spices, Pulses");
+
+      
+
+    }
+}
