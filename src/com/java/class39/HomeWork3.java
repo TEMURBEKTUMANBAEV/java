0552f0b75957a2afeb6dabd9d199be0e3571522c
Temurbek <116052673+TEMURBEKTUMANBAEV@users.noreply.github.com>
2023-03-30 08:38:03 +0300

    new file

---
+++ src/com/java/class39/HomeWork3.java
@@ -0,0 +1,18 @@
+package com.java.class39;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class HomeWork3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Set<String> words = new HashSet<>();
+
+
+        System.out.println("Enter any String");
+        String str = scanner.nextLine();
+
+        
+    }
+}
