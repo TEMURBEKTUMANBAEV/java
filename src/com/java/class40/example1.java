f9c3b5346775c8f727433a2f156312ab1121f740
Temurbek <116052673+TEMURBEKTUMANBAEV@users.noreply.github.com>
2023-03-31 10:34:35 +0300

    Update example1.java

--- src/com/java/class40/example1.java
+++ src/com/java/class40/example1.java
@@ -3,5 +3,6 @@ package com.java.class40;
 public class example1 {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hello World");
     }
 }
